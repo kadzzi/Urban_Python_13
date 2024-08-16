262156ff730962d0eff1189f3a79769185c7b879
Dmitry <kadzzi@gmail.com>
2024-08-16 20:56:44 +0300

    edited module_13_3.py deleted api

--- module_13_3.py
+++ module_13_3.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = '7040296994:AAFJGBaBwDB2XZDZPePyjUyudslo89vK714'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
