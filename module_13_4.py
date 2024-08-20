05d99d536d91716af58d7468dad36094cd477ac7
Dmitry <kadzzi@gmail.com>
2024-08-20 15:57:06 +0300

    edited module_13_4.py deleted token

--- module_13_4.py
+++ module_13_4.py
@@ -3,7 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
-api = '7040296994:AAG5p0D6CeRczU_rNhDnTKZelgobUUlJ4tQ'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
